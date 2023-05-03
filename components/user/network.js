0e74de866b84d4aa2b74c6f3f1052b257cc9e479
Ronald803 <ronaldblancobalboa@gmail.com>
2023-05-02 23:34:23 -0400

    add user post request configured

--- components/user/network.js
+++ components/user/network.js
@@ -1,16 +1,17 @@
 const express       = require('express');
 const router        = express.Router();
 const controller    = require('./controller')
+const response      = require('../../network/response')
 
 router.post('/',(req,res)=>{
     const {name,username,email,professor,tutor,projectTitle,mention,courts,password} = req.body
     //console.log({email},{password});
     controller.addUser(name,username,email,professor,tutor,projectTitle,mention,courts,password)
         .then( newUser => {
-            res.send(newUser)
+            response.success(req,res,"usuario registrado correctamente",newUser,200)
         })
         .catch( e => {
-            res.send(e)
+            response.error(req,res,"Algo salió mal",400,e)
         })
 })
 
