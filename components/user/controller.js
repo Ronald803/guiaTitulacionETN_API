c767c874310a9e0d57e2eeafec121a8beee100b7
Ronald803 <ronaldblancobalboa@gmail.com>
2023-05-07 18:46:34 -0400

    add user bugs fixed

--- components/user/controller.js
+++ components/user/controller.js
@@ -1,8 +1,10 @@
-const store = require('./store')
-const bcryptjs      = require('bcryptjs')
+const store     = require('./store')
+const bcryptjs  = require('bcryptjs')
+const jwt       = require('jsonwebtoken')
 
 function addUser(name,username,email,professor,tutor,projectTitle,mention,courts,password){
     return new Promise( async (resolve,reject)=>{
+        if(!name || !email || !mention || !password){return reject('Incomplete Data')}
         //______________ encrypting password ______________
         const salt = bcryptjs.genSaltSync();
         const encryptPassword = bcryptjs.hashSync( password,salt )
@@ -15,9 +17,13 @@ function addUser(name,username,email,professor,tutor,projectTitle,mention,courts
             progress: []
         } 
         const userSaved = await store.add(newUser)
+        // ________________ Generating jwtoken _____________
+        const payload = {uid: userSaved._id}
+        const token = jwt.sign(payload,process.env.SECRETORPRIVATEKEY,{expiresIn: '4h'})
         resolve({
-            msg:"add user desde controller",
-            userSaved
+            name: userSaved.name,
+            rol: userSaved.rol,
+            token
         })
     } )
 }
